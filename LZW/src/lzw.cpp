4006ef3c0a5f79eba33af166905b8553fb4e2ab7
brabreda <bram.bfh@gmail.com>
2022-11-24 20:04:58 +0100

    LZW decompressie start

--- LZW/src/lzw.cpp
+++ LZW/src/lzw.cpp
@@ -2,7 +2,9 @@
 #include "bitstream.h"
 #include "math.h"
 #include <map>
+#include <vector>
 #include "unordered_map"
+#include <iostream>
 
 
 constexpr int DATABITS = 7;
@@ -25,7 +27,6 @@ void initialiseer_encodeer_tabel(std::unordered_map<string, int> &tabel)
 std::string compress_lzw(const std::string &input)
 {
     obitstream out;
-
     // vul de tabel de normale karakters
     std::unordered_map<string, int> tabel;
     initialiseer_encodeer_tabel(tabel);
@@ -35,42 +36,85 @@ std::string compress_lzw(const std::string &input)
 
         std::string s;
         s.push_back(input.at(i));
-        s.push_back(input.at(i+1));
+        if(i+1 < input.size()){
+            s.push_back(input.at(i+1));
 
+            if(tabel.count(s) != 0){
 
-        if(tabel.count(s) != 0){
-            int x = 2;
-            while(tabel.count(s) != 0 && i + x < input.size()){
-                s.push_back(input.at(i+x));
-                x +=1;
+                int x = 2;
+                while(tabel.count(s) > 0 && i + x < input.size()){
+                    s.push_back(input.at(i+x));
+                    x +=1;
+                }
+
+                if( (i + x) != input.size()){
+                    tabel[s] = tabel_index;
+                    s.pop_back();
+                    i += x-2;
+                } else {
+                    i += x;
+                }
+
+                out.schrijfbits(tabel[s] , NUMBITS);
+                tabel_index++;
+
+            } else {
+                tabel[s] = tabel_index;
+
+                s.pop_back();
+                out.schrijfbits(tabel[s] , NUMBITS);
+                
+                tabel_index++;
             }
-
-            tabel[s] = tabel_index;
-            s.pop_back();
-            out.schrijfbits(tabel[s] , NUMBITS);
-            i += x;
-            tabel_index++;
-
         } else {
-            tabel[s] = tabel_index;
-            s.pop_back();
             out.schrijfbits(tabel[s] , NUMBITS);
-            
-            tabel_index++;
         }
-
     }
+    out.schrijfbits((1 << DATABITS), 12);
 
     out.flush();
     return out.str();
 }
 
+void initialiseer_decodeer_tabel(std::unordered_map<int, string > &tabel, std::vector<std::string> &woordenlijst)
+{
+    for (int i = 0; i < (1 << DATABITS); i++)
+    {
+        string s;
+        s += char(i);
+        woordenlijst.push_back(s);
+        tabel[i] = s;
+    }
+}
+
 std::string decompress_lzw(const std::string &input)
 {
+    std::unordered_map<int, string> m;
+    std::vector<std::string> woordenlijst;
+    
+    initialiseer_decodeer_tabel(m, woordenlijst);
+
+    int map_index = (1 << DATABITS) + 1;
 
     ibitstream ibs(input);
+    uint32_t bits = ibs.leesbits(12);
 
-    // VUL AAN
+    std::string s;
+
+    while( bits != 80 ){
+        s.append(m[int(bits)]);
+
+        if( std::find(woordenlijst.begin(), woordenlijst.end(), s) == woordenlijst.end()){
+            
+            m[bits] = s;
+            map_index ++;
+
+            s = "";
+        }
+
+        
+        bits = ibs.leesbits(12);
+    }
 
     return "";
 }
